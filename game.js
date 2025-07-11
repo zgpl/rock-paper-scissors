48be6211fa2cd16ba3c7b7ae4d4197bbcf688734
Zaina <zeinaeabdelfattah@gmail.com>
2025-07-11 14:21:08 +0100

    forgot to add a real title

--- game.js
+++ game.js
@@ -1,25 +1,4 @@
 let computerScore = 0;
 let humanScore = 0;
 
-function getComputerChoice() {
-    const computerChoice = Math.floor (Math.random()* 3);
-
-    if (computerChoice === 0) {
-       return "rock"
-    }
-    else if (computerChoice === 1) {
-       return "paper"
-    }
-    else if (computerChoice === 2) {
-     return "scissors"
-    }
-}
-
-console.log ("Computer choice =", getComputerChoice());
-
-function getHumanChoice () {
-    let humanChoice = prompt ("What do you say? Rock, paper or scissors?");
-    return humanChoice;
-}
-
-console.log ("Human choice =", getHumanChoice());
+function getComputerCh
